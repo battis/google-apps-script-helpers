781748215c1ff9fb19ee24062a8c3c8e773dc935
Seth Battis <SBattis@ml-tc-21-sbattis.local>
2023-01-26 20:59:47 +0000

    More concise helpers

--- src/Helper/SpreadsheetApp.ts
+++ src/Helper/SpreadsheetApp.ts
@@ -1,20 +1,19 @@
 export default class HelperSpreadsheetApp {
-
-    public static readonly CHAR = 'CHAR';
-    public static readonly FILTER = 'FILTER';
-    public static readonly IF = 'IF';
-    public static readonly IFNA = 'IFNA';
-    public static readonly INDEX = 'INDEX';
-    public static readonly JOIN = 'JOIN';
-    public static readonly MATCH = 'MATCH';
-    public static readonly SORT = 'SORT';
-    public static readonly UNIQUE = 'UNIQUE';
-
     public static fcn(name: string, ...args): string {
-        return `${name}(${args.join(',')})`
+        return `${name}(${args.join(',')})`;
     }
 
     public static eq(a: string, b: string, stringify = true): string {
         return `${a}=${stringify ? JSON.stringify(b) : b}`;
     }
+
+    public static char = this.fcn.bind('CHAR');
+    public static filter = this.fcn.bind('FILTER');
+    public static if = this.fcn.bind('IF');
+    public static ifna = this.fcn.bind('IFNA');
+    public static index = this.fcn.bind('INDEX');
+    public static join = this.fcn.bind('JOIN');
+    public static match = this.fcn.bind('MATCH');
+    public static sort = this.fcn.bind('SORT');
+    public static unique = this.fcn.bind('UNIQUE');
 }
