35712bc4edaf064ac2f56ab44f432dfdae61027c
Seth Battis <SBattis@ml-tc-21-sbattis.local>
2023-01-27 20:59:13 +0000

    addImportrangePermission

--- src/Helper/SpreadsheetApp.ts
+++ src/Helper/SpreadsheetApp.ts
@@ -16,4 +16,24 @@ export default class HelperSpreadsheetApp {
     public static match = this.fcn.bind(null, 'MATCH');
     public static sort = this.fcn.bind(null, 'SORT');
     public static unique = this.fcn.bind(null, 'UNIQUE');
+
+    /**
+     * Undocumented permissions API call
+     * @see https://stackoverflow.com/a/68498535
+     */
+    public static addImportrangePermission(
+        targetSpreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet,
+        sourceSpreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet
+    ) {
+        UrlFetchApp.fetch(
+            `https://docs.google.com/spreadsheets/d/${targetSpreadsheet.getId()}/externaldata/addimportrangepermissions?donorDocId=${sourceSpreadsheet.getId()}`,
+            {
+                method: 'post',
+                headers: {
+                    Authorization: `Bearer ${ScriptApp.getOAuthToken()}`,
+                },
+                muteHttpExceptions: true,
+            }
+        );
+    }
 }
