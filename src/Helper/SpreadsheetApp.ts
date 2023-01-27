b4eecd9a04dfd321462a7382739cd6f281e82152
Seth Battis <SBattis@ml-tc-21-sbattis.local>
2023-01-27 15:30:54 +0000

    this

--- src/Helper/SpreadsheetApp.ts
+++ src/Helper/SpreadsheetApp.ts
@@ -7,13 +7,13 @@ export default class HelperSpreadsheetApp {
         return `${a}=${stringify ? JSON.stringify(b) : b}`;
     }
 
-    public static char = this.fcn.bind('CHAR');
-    public static filter = this.fcn.bind('FILTER');
-    public static if = this.fcn.bind('IF');
-    public static ifna = this.fcn.bind('IFNA');
-    public static index = this.fcn.bind('INDEX');
-    public static join = this.fcn.bind('JOIN');
-    public static match = this.fcn.bind('MATCH');
-    public static sort = this.fcn.bind('SORT');
-    public static unique = this.fcn.bind('UNIQUE');
+    public static char = this.fcn.bind(null, 'CHAR');
+    public static filter = this.fcn.bind(null, 'FILTER');
+    public static if = this.fcn.bind(null, 'IF');
+    public static ifna = this.fcn.bind(null, 'IFNA');
+    public static index = this.fcn.bind(null, 'INDEX');
+    public static join = this.fcn.bind(null, 'JOIN');
+    public static match = this.fcn.bind(null, 'MATCH');
+    public static sort = this.fcn.bind(null, 'SORT');
+    public static unique = this.fcn.bind(null, 'UNIQUE');
 }
