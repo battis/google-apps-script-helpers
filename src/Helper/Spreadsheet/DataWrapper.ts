adf27aff766e40141aa8509ff72a8b3131096a1a
Seth Battis <seth@battis.net>
2023-02-01 21:13:27 +0000

    More configurable DataWrapper

--- src/Helper/Spreadsheet/DataWrapper.ts
+++ src/Helper/Spreadsheet/DataWrapper.ts
@@ -1,18 +1,25 @@
 export default class DataWrapper {
   private spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet;
+  private sheet?: GoogleAppsScript.Spreadsheet.Sheet;
 
   public constructor(
-    spreadsheet: string | GoogleAppsScript.Spreadsheet.Spreadsheet
+    spreadsheet: string | GoogleAppsScript.Spreadsheet.Spreadsheet,
+    sheet: string
   ) {
     if (typeof spreadsheet == 'string') {
       this.spreadsheet = SpreadsheetApp.openById(spreadsheet);
     } else {
       this.spreadsheet = spreadsheet;
     }
+    if (sheet) {
+      this.sheet = this.spreadsheet.getSheetByName(sheet);
+    }
   }
 
-  public getSheetData(name: string, range?: string) {
-    const sheet = this.spreadsheet.getSheetByName(name);
+  private getDataFrom(
+    sheet: GoogleAppsScript.Spreadsheet.Sheet,
+    range?: string
+  ) {
     if (!range) {
       return sheet.getSheetValues(
         1,
@@ -22,5 +29,16 @@ export default class DataWrapper {
       );
     }
     return sheet.getRange(range).getValues();
+  }
+
+  public getSheetData(name: string, range?: string) {
+    return this.getDataFrom(this.spreadsheet.getSheetByName(name), range);
+  }
+
+  public getData(range?: string) {
+    if (!this.sheet) {
+      throw new Error('no sheet assigned');
+    }
+    return this.getDataFrom(this.sheet, range);
   }
 }
