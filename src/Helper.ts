997271ff53d435be130ee1497a2f9600a7ad15d4
Seth Battis <seth@battis.net>
2023-02-01 16:54:58 +0000

    More complexity, why not?

--- src/Helper.ts
+++ src/Helper.ts
@@ -1,5 +1,14 @@
 import HelperSpreadsheetApp from './Helper/SpreadsheetApp';
-import HelperDriveApp from './Helper/DriveApp';
+import HelperDriveApp, { Permission as P } from './Helper/DriveApp';
+
+export namespace HelperTypes {
+  export namespace DriveApp {
+    export namespace Permission {
+      export type Role = P.Role;
+      export type Type = P.Type;
+    }
+  }
+}
 
 export default class Helper {
   public static SpreadsheetApp = HelperSpreadsheetApp;
