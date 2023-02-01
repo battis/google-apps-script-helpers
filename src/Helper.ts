24a6a09b10a3ceccea278a01df2e70c2caf0f406
Seth Battis <seth@battis.net>
2023-02-01 15:34:34 +0000

    Add permissions silently

--- src/Helper.ts
+++ src/Helper.ts
@@ -1,5 +1,7 @@
 import HelperSpreadsheetApp from './Helper/SpreadsheetApp';
+import HelperDriveApp from './Helper/DriveApp';
 
 export default class Helper {
-    public static SpreadsheetApp = HelperSpreadsheetApp;
+  public static SpreadsheetApp = HelperSpreadsheetApp;
+  public static DriveApp = HelperDriveApp;
 }
---
+++ src/Helper/DriveApp.ts
@@ -0,0 +1,40 @@
+namespace Permission {
+  export type Role =
+    | 'owner'
+    | 'organizer'
+    | 'fileOrganizer'
+    | 'writer'
+    | 'reader';
+  export type Type = 'user' | 'group' | 'domain' | 'anyone';
+}
+export default class HelperDriveApp {
+  /**
+   * Add write permission for the given user to the given document.
+   *
+   * Uses the Advanced Drive Service, which must be enabled.
+   *
+   * from: http://stackoverflow.com/a/37289790/1677912
+   *
+   * @param {string} fileId      Document ID to add permissions to.
+   * @param {string} email  Email address of user to be given editor privileges.
+   */
+  public static addPermission(
+    fileId: string,
+    email: string,
+    role: Permission.Role = 'writer',
+    type: Permission.Type = 'user',
+    optionalArgs = { sendNotificationEmails: false }
+  ) {
+    Drive.Permissions.insert(
+      {
+        role,
+        type,
+        value: email,
+      },
+      fileId,
+      {
+        ...optionalArgs,
+      }
+    );
+  }
+}
