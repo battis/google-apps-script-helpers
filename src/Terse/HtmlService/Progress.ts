7691f0068379ec5076c143a63996d0053b7cffe9
Seth Battis <seth@battis.net>
2023-02-04 17:41:47 +0000

    Oy

--- src/Terse/HtmlService/Progress.ts
+++ src/Terse/HtmlService/Progress.ts
@@ -2,6 +2,10 @@ import C from '../CacheService';
 
 export default class Progress {
   private key: string;
+  private status?: string;
+  private value: number;
+  private max: number;
+  private complete: boolean;
 
   private prefix(token: string, delimiter: string = '.') {
     return ['battis', 'Terse', 'HtmlService', 'Progress', this.key, token].join(
@@ -9,39 +13,23 @@ export default class Progress {
     );
   }
 
-  private get status() {
-    return this.prefix('status');
-  }
-
-  private get value() {
-    return this.prefix('value');
-  }
-
-  private get max() {
-    return this.prefix('max');
-  }
-
-  private get complete() {
-    return this.prefix('complete');
-  }
-
   private get html() {
     return this.prefix('html');
   }
 
   private get(key: string) {
-    return C.getUserCache(key);
+    return C.getUserCache(this.prefix(key));
   }
 
   private put(key: string, value: any) {
     return C.putUserCache(
-      key,
+      this.prefix(key),
       typeof value == 'string' ? value : JSON.stringify(value)
     );
   }
 
   private remove(key: string) {
-    return CacheService.getUserCache().remove(key);
+    return CacheService.getUserCache().remove(this.prefix(key));
   }
 
   public constructor(key: string) {
@@ -50,29 +38,30 @@ export default class Progress {
   }
 
   private putAndUpdate(key, value) {
+    this[key] = value;
     this.put(key, value);
     this.update();
   }
 
-  public setStatus = this.putAndUpdate.bind(this, this.status);
-  public getStatus = this.get.bind(this, this.status);
+  public setStatus = this.putAndUpdate.bind(this, 'status');
+  public getStatus = this.get.bind(this, 'status');
 
-  public setValue = this.putAndUpdate.bind(this, this.value);
-  public getValue = this.get.bind(this, this.value);
+  public setValue = this.putAndUpdate.bind(this, 'value');
+  public getValue = this.get.bind(this, 'value');
 
-  public setMax = this.putAndUpdate.bind(this, this.max);
-  public getMax = this.get.bind(this, this.max);
+  public setMax = this.putAndUpdate.bind(this, 'max');
+  public getMax = this.get.bind(this, 'max');
 
-  public markComplete = this.put.bind(this, this.complete, true);
-  public markIncomplete = this.put.bind(this, this.complete, false);
-  public isComplete = this.get.bind(this, this.complete);
+  public markComplete = this.put.bind(this, 'complete', true);
+  public markIncomplete = this.put.bind(this, 'complete', false);
+  public isComplete = this.get.bind(this, 'complete');
 
-  private setHtml = this.put.bind(this, this.html);
-  public getHtml = this.get.bind(this, this.html);
+  private setHtml = this.put.bind(this, 'html');
+  public getHtml = this.get.bind(this, 'html');
 
   public reset() {
     this.markIncomplete();
-    this.remove(this.status);
+    this.remove('status');
     this.setValue(0);
   }
 
@@ -81,10 +70,10 @@ export default class Progress {
       `<div class="battis Terse HtmlService Element Progress">
         <progress
           class="progress"
-          value="${this.getValue()}"
-          max="${this.getMax()}"
+          value="${this.value}"
+          max="${this.max}"
         />
-        <div class="status">${this.getStatus()}</div>
+        <div class="status">${this.status || ''}</div>
       </div>`
     );
   }
