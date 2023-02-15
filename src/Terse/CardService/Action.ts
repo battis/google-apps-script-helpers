b51f334dd26f8d529b6f8630940414992808394d
Seth Battis <seth@battis.net>
2023-02-15 13:48:02 +0000

    Typing note

--- src/Terse/CardService/Action.ts
+++ src/Terse/CardService/Action.ts
@@ -2,27 +2,28 @@ export type UnprocessedParameters = { [key: string]: any };
 export type Parameters = { [key: string]: string };
 
 export type Action = {
-  functionName: string;
-  parameters?: UnprocessedParameters;
+    functionName: string;
+    parameters?: UnprocessedParameters;
 };
 
 export function stringify(parameters: UnprocessedParameters): Parameters {
-  for (const key of Object.keys(parameters)) {
-    if (typeof parameters[key] != 'string') {
-      parameters[key] = JSON.stringify(parameters[key]);
+    for (const key of Object.keys(parameters)) {
+        if (typeof parameters[key] != 'string') {
+            parameters[key] = JSON.stringify(parameters[key]);
+        }
     }
-  }
 
-  return parameters || {};
+    return parameters || {};
 }
 
+// FIXME fix typing to reflect requirement for functionName or openLink, etc.
 export function newAction({
-  functionName,
-  parameters = null,
+    functionName,
+    parameters = null,
 }: Action): GoogleAppsScript.Card_Service.Action {
-  const action = CardService.newAction().setFunctionName(functionName);
-  if (parameters) {
-    return action.setParameters(stringify(parameters));
-  }
-  return action;
+    const action = CardService.newAction().setFunctionName(functionName);
+    if (parameters) {
+        return action.setParameters(stringify(parameters));
+    }
+    return action;
 }
