f9243d0ffb148d120ced4d2c6f28dd1334b4507e
Seth Battis <seth@battis.net>
2023-02-16 20:20:56 +0000

    replaceWithDisplayValues

--- src/Terse/SpreadsheetApp/index.ts
+++ src/Terse/SpreadsheetApp/index.ts
@@ -1,2 +1,16 @@
 export * as DeveloperMetadata from './DeveloperMetadata';
 export * as Protection from './Protection';
+
+export function replaceWithDisplayValues(
+    range: GoogleAppsScript.Spreadsheet.Range
+) {
+    range.setValues(range.getDisplayValues());
+}
+
+export function replaceAllWithDisplayValues(
+    sheet: GoogleAppsScript.Spreadsheet.Sheet
+) {
+    replaceWithDisplayValues(
+        sheet.getRange(1, 1, sheet.getMaxRows(), sheet.getMaxColumns())
+    );
+}
