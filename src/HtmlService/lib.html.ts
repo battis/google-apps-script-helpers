951d933523f662762b8f322f628df0bb8cd06c07
Seth Battis <seth@battis.net>
2023-02-20 19:00:10 +0000

    execute script in innerHTML

--- src/HtmlService/lib.html.ts
+++ src/HtmlService/lib.html.ts
@@ -1,7 +1,19 @@
 export default `<script>
   if (!window.GAS_LIGHTER_LIB_LOADED) {
       window.replaceContent = function(html) {
-          document.getElementById('content').innerHTML = html;
+          // https://stackoverflow.com/a/47614491/294171
+          content = document.getElementById('content');
+          content.innerHTML = html;
+          Array.from(content.querySelectorAll("script"))
+            .forEach( oldScriptEl => {
+                const newScriptEl = document.createElement("script");
+                Array.from(oldScriptEl.attributes).forEach( attr => {
+                  newScriptEl.setAttribute(attr.name, attr.value)
+                });
+                const scriptText = document.createTextNode(oldScriptEl.innerHTML);
+                newScriptEl.appendChild(scriptText);
+                oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
+            });
       }
       window.attachEvent = function(element, event, handler) {
           if (element.attachEvent) {
