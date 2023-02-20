4b4909b8111b0dff32c03fa3af1bbc405ca92456
Seth Battis <seth@battis.net>
2023-02-20 18:09:54 +0000

    sigh

--- src/HtmlService/lib.html.ts
+++ src/HtmlService/lib.html.ts
@@ -1,16 +1,16 @@
 export default `<script>
   console.log('lib loaded')
-  if (!replaceContent) {
+  if (!window.replaceContent) {
       console.log('defining replaceContent()')
-      replaceContent = (html) => {
+      window.replaceContent = (html) => {
           document.getElementById('content').innerHTML = html;
       }
   } else {
       console.log('replaceContent() alreadu defined')
   }
 
-  if (!attachEvent) {
-      attachEvent = (element, event, handler) => {
+  if (!window.attachEvent) {
+      window.attachEvent = (element, event, handler) => {
           if (element.attachEvent) {
               element.attachEvent(event, handler);
           } else {
