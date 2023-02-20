659cfb8c750109591a33408ec6b6d69ac7f93409
Seth Battis <seth@battis.net>
2023-02-20 17:21:46 +0000

    le sigh

--- src/Globals.ts
+++ src/Globals.ts
@@ -2,6 +2,7 @@ import { include } from './HtmlService';
 import { getProgress } from './HtmlService/Element/Progress';
 import { dialogClose } from './UI/Dialog';
 
+// FIXME won't work without improvements in gas-webpack-plugin
 // TODO some sort of fancy filtering
 export function register() {
     global.include = include;
