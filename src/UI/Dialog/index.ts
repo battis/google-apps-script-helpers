31f2928a7f71d2808c4049d5ffc9b4d880656f7d
Seth Battis <seth@battis.net>
2023-02-20 15:52:16 +0000

    More reusable

--- src/UI/Dialog/index.ts
+++ src/UI/Dialog/index.ts
@@ -7,13 +7,15 @@ type Button = {
     value?: string;
     class?: string;
 };
-type Options = {
-    root: Root;
+type HtmlOptions = {
     message: string;
-    title: string;
     height?: number;
     buttons?: (Button | string)[];
     functionName: string;
+};
+type DialogOptions = HtmlOptions & {
+    root: Root;
+    title: string;
 };
 
 function standardizeButton(button: Button | string) {
@@ -25,26 +27,28 @@ function standardizeButton(button: Button | string) {
 
 function show(
     showFunctionName: string,
-    {
-        root,
-        message,
-        title,
-        buttons = [{ name: 'Ok' }],
-        height = 100,
-        functionName = null,
-    }: Options
+    { root, title, ...dialog }: DialogOptions
 ) {
-    root.getUi()[showFunctionName](
-        Html.createTemplate(html, {
-            message,
-            buttons: buttons.map(standardizeButton),
-            functionName,
-        }).setHeight(height),
-        title
-    );
+    root.getUi()[showFunctionName](getHtmlOutput(dialog), title);
 }
 
 export const showModal = show.bind(null, 'showModalDialog');
 export const showModeless = show.bind(null, 'showModelessDialog');
+
+export function getHtmlOutput({
+    message,
+    buttons = [{ name: 'Ok' }],
+    height = 100,
+    functionName = null,
+}: HtmlOptions) {
+    return Html.createTemplate(html, {
+        message,
+        buttons: buttons.map(standardizeButton),
+        functionName,
+    }).setHeight(height);
+}
+
+export const getHtml = (options: DialogOptions) =>
+    getHtmlOutput(options).getContent();
 
 export const close = () => null;
