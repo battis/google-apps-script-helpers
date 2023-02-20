1a6b532538f59ddd25c279747e527a1182e31c5e
Seth Battis <seth@battis.net>
2023-02-20 18:32:58 +0000

    html

--- src/UI/Dialog/content.html.ts
+++ src/UI/Dialog/content.html.ts
@@ -1,38 +1,41 @@
 export default `
 <form id="dialog_<?!= data.id ?>" >
     <div><?= data.message ?> </div>
-        < div class="bottom-right" >
-            <?for (const button of data.buttons) { ?>
-                <button
-        class="<?= button.class ?>"
-    name = "response"
-    type = "submit"
-    value = "<?= button.value ?>"
-        >
-        <?= button.name ?>
+    <div class="bottom-right">
+        <? for (const button of data.buttons) { ?>
+            <button
+                class="<?= button.class ?>"
+                name = "response"
+                type = "submit"
+                value = "<?= button.value ?>"
+            >
+                <?= button.name ?>
             </button>
-            <? } ?>
-                </div>
-                < /form>
-    <script>
-                  function handleResponse_<?!= data.id ?>(html) {
-                    if (html) {
-                      replaceContent(html)
-                    } else {
-                      google.script.host.close();
-                    }
-                  }
+        <? } ?>
+    </div>
+</form>
+<script>
 
-                  attachEvent(document.getElementById('dialog_<?!= data.id ?>'), 'submit', function(e) {
-                    if (e.preventDefault) {
-                      e.preventDefault();
-                    }
-                    for (const button of document.querySelectorAll('#dialog_<?!= data.id ?> button')) {
-                      button.disabled = true;
-                    }
-                    google.script.run.withSuccessHandler(handleResponse_<?!= data.id ?>).<?!= data.functionName ?>(e.submitter.value);
-                    return false;
-                  });
-                </script>
+function handleResponse_<?!= data.id ?>(html) {
+    if (html) {
+        replaceContent(html);
+    } else {
+        google.script.host.close();
+    }
+}
 
+attachEvent(document.getElementById('dialog_<?!= data.id ?>'), 'submit', function(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+    for (const button of document.querySelectorAll('#dialog_<?!= data.id ?> button')) {
+        button.disabled = true;
+    }
+    google.script.run
+        .withSuccessHandler(handleResponse_<?!= data.id ?>)
+        .<?!= data.functionName ?>(e.submitter.value);
+    return false;
+});
+
+</script>
 `;
