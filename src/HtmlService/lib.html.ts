40920439cf362ebdd2ec50f1e1f90286843852e0
Seth Battis <seth@battis.net>
2023-02-20 18:01:41 +0000

    safer

--- src/HtmlService/lib.html.ts
+++ src/HtmlService/lib.html.ts
@@ -1,13 +1,17 @@
 export default `<script>
-  function replaceContent(html) {
-    document.getElementById('content').innerHTML = html;
+  if (!replaceContent) {
+      replaceContent = (html) => {
+          document.getElementById('content').innerHTML = html;
+      }
   }
 
-  function attachEvent(element, event, handler) {
-    if (element.attachEvent) {
-      element.attachEvent(event, handler);
-    } else {
-      element.addEventListener(event, handler);
-    }
+  if (!attachEvent) {
+      attachEvent = (element, event, handler) => {
+          if (element.attachEvent) {
+              element.attachEvent(event, handler);
+          } else {
+              element.addEventListener(event, handler);
+          }
+      }
   }
 </script>`;
