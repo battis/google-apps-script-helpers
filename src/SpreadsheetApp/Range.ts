9bbaa6f93baf68006563415421e7b4c60d3a3328
Seth Battis <seth@battis.net>
2023-03-08 14:46:35 +0000

    getEntireSheet()

---
+++ src/SpreadsheetApp/Range.ts
@@ -0,0 +1,2 @@
+export const getEntireSheet = (sheet: GoogleAppsScript.Spreadsheet.Sheet) =>
+    sheet.getRange(1, 1, sheet.getMaxRows(), sheet.getMaxColumns());
--- src/SpreadsheetApp/index.ts
+++ src/SpreadsheetApp/index.ts
@@ -3,15 +3,17 @@ import * as DeveloperMetadata_module from './DeveloperMetadata';
 import * as Function_module from './Function';
 import * as Permission_module from './Permission';
 import * as Protection_module from './Protection';
+import * as Range_module from './Range';
 import * as Value_module from './Value';
 
 namespace SpreadsheetApp_namespace {
+    export import DeveloperMetadata = DeveloperMetadata_module;
     export const Dialog =
         Dialog_module.bindTo<typeof SpreadsheetApp>(SpreadsheetApp);
     export import Function = Function_module;
     export import Permission = Permission_module;
-    export import Value = Value_module;
-    export import DeveloperMetadata = DeveloperMetadata_module;
     export import Protection = Protection_module;
+    export import Range = Range_module;
+    export import Value = Value_module;
 }
 export default SpreadsheetApp_namespace;
