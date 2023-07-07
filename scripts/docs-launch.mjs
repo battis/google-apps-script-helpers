9af48bf1a3805ba9b70347edda0949c7b526aaae
Seth Battis <seth@battis.net>
2023-07-06 21:31:54 -0400

    automate docs tagging

--- scripts/docs-launch.mjs
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import open from 'open';
-open('./docs/docs/index.html');
--- scripts/docs-publish.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import appRootPath from 'app-root-path';
-import path from 'path';
-import shell from 'shelljs';
-
-shell.cd(path.join(appRootPath.toString(), 'docs'));
-shell.exec('git add -A .');
-shell.exec('git commit -m "Update documentation"');
-shell.exec('git push');
