7a2e9ca64ae38724778f9d5be87081088ba8a5b2
Seth Battis <sbattis@ml-tc-21-sbattis.local>
2023-10-22 17:14:01 -0400

    clean dependencies

--- bin/docs/publish.mjs
+++ bin/docs/publish.mjs
@@ -2,15 +2,15 @@
 import appRootPath from 'app-root-path';
 import fs from 'fs';
 import path from 'path';
-import shell from 'shelljs';
+import cli from '@battis/qui-cli';
 
 const pkg = JSON.parse(
   fs.readFileSync(new URL('../../package.json', import.meta.url))
 );
 const tag = `v${pkg.version}-docs`;
 
-shell.cd(path.join(appRootPath.toString(), './docs'));
-shell.exec('git add -A .');
-shell.exec(`git commit -m "Update documentation for v${pkg.version}"`);
-shell.exec(`git tag ${tag}`);
-shell.exec(`git push origin ${tag}`);
+cli.shell.cd(path.join(appRootPath.toString(), './docs'));
+cli.shell.exec('git add -A .');
+cli.shell.exec(`git commit -m "Update documentation for v${pkg.version}"`);
+cli.shell.exec(`git tag ${tag}`);
+cli.shell.exec(`git push origin ${tag}`);
