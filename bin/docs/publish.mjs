ee87ff371fbb122bb2ac9df368de64d0770ad1c6
Seth Battis <seth@battis.net>
2024-08-07 14:17:05 -0400

    prep bin for monorepo

--- bin/docs/publish.mjs
+++ bin/docs/publish.mjs
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
-import cli from '@battis/qui-cli';
 import appRootPath from 'app-root-path';
 import fs from 'fs';
 import path from 'path';
+import shell from 'shelljs';
 
 const pkg = JSON.parse(
   fs.readFileSync(new URL('../../package.json', import.meta.url))
 );
 const tag = `v${pkg.version}-docs`;
 
-cli.shell.cd(path.join(appRootPath.toString(), './docs'));
-cli.shell.exec('git add -A .');
-cli.shell.exec(`git commit -m "Update documentation for v${pkg.version}"`);
-cli.shell.exec(`git tag ${tag}`);
-cli.shell.exec(`git push origin ${tag}`);
+shell.cp(path.join(appRootPath.toString(), '/assets/*'), './docs/assets', 'r');
+shell.cd(path.join(appRootPath.toString(), './docs'));
+shell.exec('git add -A .');
+shell.exec(`git commit -m "Update documentation for v${pkg.version}"`);
+shell.exec(`git tag ${tag}`);
+shell.exec(`git push origin ${tag}`);
